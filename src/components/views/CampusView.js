334c2693a850556c5341d2c3404281a38307c256
xchen <xchen601@gmail.com>
2023-11-26 01:29:24 -0500

    delete campus

--- src/components/views/CampusView.js
+++ src/components/views/CampusView.js
@@ -8,7 +8,7 @@ import { Link } from "react-router-dom";
 
 // Take in props data to construct the component
 const CampusView = (props) => {
-  const {campus} = props;
+  const {campus, handleDelete} = props;
   
   // Render a single Campus view with list of its students
   return (
@@ -26,6 +26,7 @@ const CampusView = (props) => {
           </div>
         );
       })}
+      <button onClick={handleDelete}>Delete</button>
     </div>
   );
 };
